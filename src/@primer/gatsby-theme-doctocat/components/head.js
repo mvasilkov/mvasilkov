14fb2bbce0fa659912b705571332139157fbcdf1
Mark Vasilkov <MarkV@nsogroup.com>
2020-09-30 00:34:22 +0300

    System Font Stacks

--- src/@primer/gatsby-theme-doctocat/components/head.js
+++ src/@primer/gatsby-theme-doctocat/components/head.js
@@ -11,3 +11,6 @@ average == mix(#ff5858, #f857a6) == #fc587f
 theme.colors.rei = {
     pink: '#fc587f',
 }
+
+theme.fonts.normal = `-apple-system, 'Segoe UI', 'DejaVu Sans', system-ui, sans-serif`
+theme.fonts.mono = `'SF Mono', SFMono-Regular, ui-monospace, 'DejaVu Sans Mono', Menlo, Consolas, monospace`
